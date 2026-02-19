0deb18d58cbe7181ee662071f7e23fae65d0e987
Rehtegot47 <emmanuelezekwu63@gmail.com>
2026-02-19 01:42:46 -0800

    first commit

---
+++ src/main/java/com/example/computation/ComputationApplication.java
@@ -0,0 +1,13 @@
+package com.example.computation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/computation/ComputationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.computation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
